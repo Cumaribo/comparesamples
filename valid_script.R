8eb99841b6f780153b374f547c520878b3e205d3
Cumaribo <38987055+Cumaribo@users.noreply.github.com>
2020-08-17 23:21:44 -0400

    Update valid_script.R
    This versionb extracts all the contingency tables for all the combinations and also creates the tibble that will be used to graph the results. Almost there.

--- valid_script.R
+++ valid_script.R
@@ -20,6 +20,7 @@ library(tidyverse)
 library(data.table)
 library(lwgeom)
 library(aRn)
+library(diffeR)
 
 setwd("~/Documents/victor_valid/Orinoquia")
 
@@ -32,8 +33,8 @@ plot(samples)
 poly1 <- st_read('~/Documents/victor_valid/Orinoquia/poly_Ori_JRE_j.shp')
 poly2 <- st_read('~/Documents/victor_valid/Orinoquia/poly_Ori_MVS_j.shp')
 
-# load template raster (for rasterization)
 
+# load template raster (for rasterization)
 
 template <- raster('template.tif')
 mat <- rclMatrix(-1796,oneFirst=FALSE)
@@ -50,109 +51,71 @@ CC <- raster("LC08_L1TP_005057_20140128_20170426_removeChgMsk8_CCA.tif")
 
 ###############################################################
 
-square_cont <- function(poly1, msk, template, samples, writeraster=FALSE, plotAgMap=TRUE){
+square_cont <- function(poly1, poly2, alg1,alg2,alg3, msk, samples, writeraster=FALSE, plotAgMap=TRUE){
   samplesID <- samples$sr_band2_1
   test1 <- poly1[poly1$sr_band2_1%in%samplesID,] 
   test2 <- poly2[poly2$sr_band2_1%in%samplesID,]
   window1 <- samples[samples$sr_band2_1%in%samplesID,]
-  test1 <- st_crop(test1, extent(window1))
+   test1 <- st_crop(test1, extent(window1))
+   test2 <- st_crop(test2, extent(window1))
   msk <- crop(msk, extent(window1))
-  template. <- crop(template,extent(window1))
-  test1.r <- rasterize(test1, template., test1$change_b)
-  m <- c(-Inf,1,0,1, Inf, 1)
-  m <- matrix(m, ncol=3,byrow=TRUE)
-  test1.r <-reclassify(test1.r,m)
-  msk[is.na(msk[])] <- 0
-  #return(list(test1.r, msk))}
+  alg1 <- crop(alg1, extent(window1))
+  alg2 <- crop(alg2, extent(window1))
+  alg3 <- crop(alg3, extent(window1))
+  test1.r <- rasterize(test1, msk, test1$change_b)
+  test2.r <- rasterize(test2, msk, test2$change_b)
+  #return(list(test1.r,test2.r))}
+  alg1[is.na(alg1[])] <- 0
+  alg2[is.na(alg2[])] <- 0
+  alg3[is.na(alg3[])] <- 0
+  #return(alg)}
   #test2.r <- rasterize(test2, template., test2$change_b)
-  crosstab1 <- crosstabm(test1.r,msk, percent=TRUE, population = NULL)
+  #claculate the cont_table between each dataset and a plain background
+  crosstab1 <- crosstabm(msk, test1.r, percent=TRUE, population = NULL)
+  crosstab2 <- crosstabm(msk, test2.r, percent=TRUE, population=NULL)
+  crosstab3 <- crosstabm(msk,alg1,percent=TRUE, population = NULL)
+  crosstab4 <- crosstabm(msk,alg2,percent=TRUE, population = NULL)
+  crosstab5 <- crosstabm(msk,alg3,percent=TRUE, population = NULL)
+  crosstab6 <- crosstabm(test1.r,test2.r,percent=TRUE, population = NULL)
   #differences <- differenceMR(test1.r,msk, eval='original', percent=TRUE)
   #overallComponentsPlot(comp = test1.r, ref = msk, ctmatrix = NULL, units = NULL,population = NULL)
-  return(crosstab)}
+  return(list(crosstab1, crosstab2, crosstab3, crosstab4, crosstab5,crosstab6))}
   
-
 #Extract Square Contingency matrices
-con_tables <- (x=1:nrow(samples))%>%map(function(x) square_cont(poly1,msk=CC, template, samples=samples[x,], writeraster=TRUE, plotAgMap =TRUE))
+con_tables <- (x=1:nrow(samples))%>%map(function(x) square_cont(poly1,poly2, alg1=CC, alg2=Gamma,alg3=CCACHsq, msk, samples=samples[x,], writeraster=TRUE, plotAgMap =TRUE))
+names(con_tables) <- samplesID
+
+
 #Caculate Difference Metrics
-tablej <- (x=1:nrow(samples))%>%map(function(x) diffTablej(con_tables[[x]][[1]], digits = 2, analysis = 'error'))
-#Caclculate Overall Differences (the inverse of the agreement) 
-overalldff <- (x=1:nrow(samples))%>%map(function(x) overallDiffCatj(results[[x]][[1]]))
+tablej1 <- (x=1:nrow(samples))%>%map(function(x) diffTablej(con_tables[[x]][[1]], digits = 2, analysis = 'error'))
+tablej2 <- (x=1:nrow(samples))%>%map(function(x) diffTablej(con_tables[[x]][[2]], digits = 2, analysis = 'error'))
+tablejCC<- (x=1:nrow(samples))%>%map(function(x) diffTablej(con_tables[[x]][[3]], digits = 2, analysis = 'error'))
+tablejgamma<- (x=1:nrow(samples))%>%map(function(x) diffTablej(con_tables[[x]][[4]], digits = 2, analysis = 'error'))
+tablejCCchsq<- (x=1:nrow(samples))%>%map(function(x) diffTablej(con_tables[[x]][[5]], digits = 2, analysis = 'error'))#Caclculate Overall Differences (the inverse of the agreement) 
+
+# calculate Overall Differences
+overalldiff1 <-(x=1:nrow(samples))%>%map(function(x) overallDiff(con_tables[[x]][[1]]))
+overalldiff2 <-(x=1:nrow(samples))%>%map(function(x) overallDiff(con_tables[[x]][[2]]))
+overalldiff_CC <-(x=1:nrow(samples))%>%map(function(x) overallDiff(con_tables[[x]][[3]]))
+overalldiff_gamma <-(x=1:nrow(samples))%>%map(function(x) overallDiff(con_tables[[x]][[4]]))
+overalldiff_CCchsq <-(x=1:nrow(samples))%>%map(function(x) overallDiff(con_tables[[x]][[5]]))
+overalldiff1_2 <-(x=1:nrow(samples))%>%map(function(x) overallDiff(con_tables[[x]][[6]]))
 
 
-tablejj <- do.call(rbind, tablej)
+#Assign the IDs from Samples
+names(overalldiff1) <- samplesID
+names(overalldiff2) <- samplesID
+names(overalldiff_CC) <- samplesID
+names(overalldiff_gamma) <- samplesID
+names(overalldiff_CCchsq) <- samplesID
+names(overalldiff1_2) <- samplesID
 
 
-results[[1]]
+#bind the data together and convert into a tibble
+binded <- cbind(overalldiff1,overalldiff2,overalldiff_CC,overalldiff_gamma,overalldiff_CCchsq,overalldiff1_2)
+binded <- as.data.frame(binded)
+binded <- as_tibble(binded)
 
-tablej[[1]]
-overalldff[[1]]
-results[[1]]
-
-
-
-
-agreement <- (x=1:nrow(samples))%>%map(function(x) agreementj(results[[x]][[1]]))
-
-
-
-omissionm <-(x=1:nrow(samples))%>%map(function(x) omissionj(results[[x]][[1]]))
-comission.<-(x=1:nrow(samples))%>%map(function(x) comissionj(results[[x]][[1]]))
-
-                                       
-                                       
-                                       
-                                       
-                                       
-agreement <- (x=1:nrow(samples))%>%map(function(x) agreementj(results([[1][1]])))
-
-
-
-omission <- omissionj(results[[1]])
-comission <- comissionj(results[[1]])
-
-
-
-
-results <- (x=1:nrow(samples))%>%map(function(x) compare.r(poly1,msk=Gamma, template, samples=samples[x,], writeraster=TRUE, plotAgMap =TRUE))
-results3 <- (x=1:4)%>%map(function(x) compare.r(poly1,msk=CCACHsq, template, samples=samples[x,], writeraster=TRUE, plotAgMap =TRUE))
-plot(results[[1]]), add=TRUE)
-
-plot(results[[1]], add=TRUE)
-plot(results[[1]][[1]])
-
-plot(results[[1]][[2]], add=TRUE)
-results
-
-extent(results[[1]])
-
-plot(results[[5]])
-results <- (x=1:9)%>%map(function(x) compare.r(poly1, template, samples=samples[x,], writeraster=TRUE, plotAgMap =TRUE))
-
-samplesID[10]
-
-names(results) <- samplesID
-
-# save the results
-save(results, file= "contingencym_Orinoquia.RData")
-
-load('contingencym_DF_8.RData')
-load('contingency_m.RData')
-
-results
-
-results_rev <- results
-dir()
-results2 <- do.call(rbind, results)
-
-
-resultCCSChsq <- results2
-resultsGamma <- results2
-resultsCC <- results2
-
-summary(resultsGamma$Overall)
-summary(resultCCSChsq$Overall)
-summary(resultsCC$Overall)
-names(results)
-names(results_rev)
-
-tablej[[1]]$Agreement[3]
+ggplot(data=binded,mapping = )
+#Set the column names
+names(binded) <- c('User1', 'User2', 'CC','Gamma', 'CCChsq', 'User1/2')
