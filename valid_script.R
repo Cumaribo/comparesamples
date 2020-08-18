ff87ec0997a6bc9cab2eac4ccc6a59a077b69e75
Cumaribo <38987055+Cumaribo@users.noreply.github.com>
2020-08-17 20:21:33 -0400

    Update valid_script.R
    This updates corrects typos and generates a list of comparison metrics between the three datasets, poly1, poly2 and alg and a no_change generic raster. It also returns the agreement between the two datasets generated by the analyst

--- valid_script.R
+++ valid_script.R
@@ -15,13 +15,13 @@ library(rgdal)
 library(sf)
 library(rgeos)
 library(raster)
-library(greenbrown)
 library(tidyverse)
-library(data.table)
 library(lwgeom)
+libary(diffeR)
 library(aRn)
 
-setwd("~/Documents/victor_valid/Orinoquia")
+
+setwd("~/path/to/your/folder")
 
 # load study windows
 dir()
@@ -33,7 +33,6 @@ poly1 <- st_read('~/Documents/victor_valid/Orinoquia/poly_Ori_JRE_j.shp')
 poly2 <- st_read('~/Documents/victor_valid/Orinoquia/poly_Ori_MVS_j.shp')
 
 # load template raster (for rasterization)
-
 
 template <- raster('template.tif')
 mat <- rclMatrix(-1796,oneFirst=FALSE)
@@ -51,47 +50,34 @@ CC <- raster("LC08_L1TP_005057_20140128_20170426_removeChgMsk8_CCA.tif")
 samplesID[5]
 ###############################################################
 
-compare.r <- function(poly1, msk, template, samples, writeraster=FALSE, plotAgMap=TRUE){
+square_cont <- function(poly1, poly2, alg, msk, samples, writeraster=FALSE, plotAgMap=TRUE){
   samplesID <- samples$sr_band2_1
   test1 <- poly1[poly1$sr_band2_1%in%samplesID,] 
   test2 <- poly2[poly2$sr_band2_1%in%samplesID,]
   window1 <- samples[samples$sr_band2_1%in%samplesID,]
-  test1 <- st_crop(test1, extent(window1))
+   test1 <- st_crop(test1, extent(window1))
+   test2 <- st_crop(test2, extent(window1))
   msk <- crop(msk, extent(window1))
-  template. <- crop(template,extent(window1))
-  test1.r <- rasterize(test1, template., test1$change_b)
-  m <- c(-Inf,1,0,1, Inf, 1)
-  m <- matrix(m, ncol=3,byrow=TRUE)
-  test1.r <-reclassify(test1.r,m)
-  msk[is.na(msk[])] <- 0
-  #return(list(test1.r, msk))}
-  #test2.r <- rasterize(test2, template., test2$change_b)
-  crosstab <- crosstabm(test1.r,msk, percent=TRUE, population = NULL)
-  differences <- differenceMR(test1.r,msk, eval='original', percent=TRUE)
-  #overallComponentsPlot(comp = test1.r, ref = msk, ctmatrix = NULL, units = NULL,population = NULL)
-  #return(crosstab)}
-  #return(differences)}
-  return(list(crosstab, differences))}
+  alg <- crop(alg, extent(window1))
+  test1.r <- rasterize(test1, msk, test1$change_b2)
+  test2.r <- rasterize(test2, msk, test2$change_b2)
+  alg[is.na(alg[])] <- 0
+  crosstab1 <- crosstabm(msk, test1.r, percent=TRUE, population = NULL)
+  crosstab2 <- crosstabm(msk, test2.r, percent=TRUE, population=NULL)
+  crosstab3 <- crosstabm(msk,alg,percent=TRUE, population = NULL)
+  crosstab4 <- crosstabm(test1.r,test2.r,percent=TRUE, population = NULL)
+  #differences <- differenceMR(test1.r,msk, eval='original', percent=TRUE)
+  return(list(crosstab1, crosstab2, crosstab3, crosstab4))}
   
-
-#MADscatterplot(test1.r, msk, strata = template.)
-  comparedata <- CompareClassification(test1.r, msk, names = list('Ref'=c('no-change','change'),'chg_msk'=c('no-change','change')), samplefrac = 1)
-  if(plotAgMap==TRUE){
-  plot(comparedata)}
-  return(comparedata)}
-
-
-  if(writeraster==TRUE){
-    writeRaster(comparedata$raster, paste(samplesID, "agreement",sep='_'), format='GTiff', overwrite=TRUE)
-  }}
-res<- as.data.frame(comparedata$table)
-  #names(res) <- samplesID
-  colnames(res) <- c('no-change', 'change', 'Sum','UserAccuracy')
-  #names(res) <- samplesID
-  return(res)
-  #return(comparedata)
-}
-
+# Square Contingency matrices
+con_tables <- (x=1:nrow(samples))%>%map(function(x) square_cont(poly1,poly2, alg=CC, msk, samples=samples[x,], writeraster=TRUE, plotAgMap =TRUE))
+names(con_tables) <- samplesID
+  
+#Caculate Difference Metrics
+tablej <- (x=1:nrow(samples))%>%map(function(x) diffTablej(con_tables[[x]][[1]], digits = 2, analysis = 'error'))
+tablej2 <- (x=1:nrow(samples))%>%map(function(x) diffTablej(con_tables[[x]][[2]], digits = 2, analysis = 'error'))
+#Caclculate Overall Differences (the inverse of the agreement) 
+overalldff <- (x=1:nrow(samples))%>%map(function(x) overallDiffCatj(results[[x]][[1]]))
 # Run the function
 
 results <- (x=1:1)%>%map(function(x) compare.r(poly1,msk=CC, template, samples=samples[x,], writeraster=TRUE, plotAgMap =TRUE))
@@ -120,45 +106,3 @@ comission <- comissionj(results[[1]])
 
 
 
-
-results <- (x=1:nrow(samples))%>%map(function(x) compare.r(poly1,msk=Gamma, template, samples=samples[x,], writeraster=TRUE, plotAgMap =TRUE))
-results3 <- (x=1:4)%>%map(function(x) compare.r(poly1,msk=CCACHsq, template, samples=samples[x,], writeraster=TRUE, plotAgMap =TRUE))
-plot(results[[1]]), add=TRUE)
-
-plot(results[[1]], add=TRUE)
-plot(results[[1]][[1]])
-
-plot(results[[1]][[2]], add=TRUE)
-results
-
-extent(results[[1]])
-
-plot(results[[5]])
-results <- (x=1:9)%>%map(function(x) compare.r(poly1, template, samples=samples[x,], writeraster=TRUE, plotAgMap =TRUE))
-
-samplesID[10]
-
-names(results) <- samplesID
-
-# save the results
-save(results, file= "contingencym_Orinoquia.RData")
-
-load('contingencym_DF_8.RData')
-load('contingency_m.RData')
-
-results
-
-results_rev <- results
-dir()
-results2 <- do.call(rbind, results)
-
-
-resultCCSChsq <- results2
-resultsGamma <- results2
-resultsCC <- results2
-
-summary(resultsGamma$Overall)
-summary(resultCCSChsq$Overall)
-summary(resultsCC$Overall)
-names(results)
-names(results_rev)
