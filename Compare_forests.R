56f345ef2cb2f5e8085bbcb790268dfadf96fdc5
Cumaribo <38987055+Cumaribo@users.noreply.github.com>
2020-09-01 22:22:19 -0400

    Update Compare_forests.R

--- Compare_forests.R
+++ Compare_forests.R
@@ -1,8 +1,7 @@
-# This code is used to compare between different maps,
-# returning a set of contingence matrices that provide information about the level of agreement
-# as well as telling the quantity, shift, exchange charactetistics of such changes
-# it is designed to return some maps and contingency matrices
-
+# This code is used to download all the forest- no forest binary masks from the 
+# Global forest cover Dataset by Hansen et al  using the package ForestChange.
+#It  returns a list containiing the succesive masks for the requested period and spatial extent
+# given the Canopy cover threshold.
 #It requires the package "greenbrown" available here:
 http://greenbrown.r-forge.r-project.org
 # Load libraries 
@@ -15,18 +14,12 @@ library(rgeos)
 library(diffeR)
 library(parallel)
 
-
-
-
 #Set your working folder
 setwd("~/path/to/your/folder")
 
-
-classified_map <- raster('your_classifiedmap.tif')
-
-
 #you will need a shapefile of your study area. You can vectorize the mosaic or 
-#any of the masks for that. Make sure 
+#any of the masks provided for that. You can also use the getGADM() function of the package 
+#forestChange for that purpose.  
 vectorname <- 'vectorfile'
  mun <- readOGR('~/path/to/yorufolder', vectorname)
  #make sure that your vectorfile is in crs=WGS84, as this is the one of the gobal forest dataset.
@@ -34,26 +27,26 @@ vectorname <- 'vectorfile'
 # if necessary, use this to reproject:
  #mun <- spTransform(mun, crs='+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0')
 
-# i needed to do this for some reason. 
 Sys.setlocale(locale="C")
 
-#create a list of succesive years
-years <- list(2000:2020)
+#create a list of succesive years. Note. As of today, August 31 2020, the latest year for which
+#Hansen Global data is available is 2018. Exp
+#available up t
+years <- list(2000:2018)
 years <- as.data.frame(years)
 #FCmask creates a binary forest/no-forest mask for a given year and for the selected polygon.
 #run the FCMask function for each year. Set 95 as the threshold. the "mun" argument defines the mask to clip and
 # download the forest data 
 #the mc.cores argument tells R to run the process using all the available cores in the computer 
-#simultaneously. It's way faster. You only limitation  is the available memory. 
-#for one single year, the function looks like this.
+#simultaneously.The only limitation  is the available memory. 
+#for one single year, the function looks like this:
 
 forest1 <- FCMask(mun, year=2000, cummask=FALSE, perc=95:100, mc.cores = detectCores())
-years[1][2]
 
-#you will have to run for each one of the years. However we're lazy people and want the computer
-# to work for us. So we are going to map the function to iterate for the whole *years* list. It returns a list 
+¡
+#you will have to run for each one of the years. However we can auotmatize the process
+# and let the computer work for us. So we are going to map the function to iterate for the whole *years* list. It returns a list 
 # with all the rasters 
-
 
 forests <- (x=1:nrow(years))%>%map(function(x) FCMask(mun, year=years[x,1], cummask=FALSE, perc=95:100, mc.cores = detectCores()))
 #create binary forest mask with forestChange 
